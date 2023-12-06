73b1d3805015b5df5d8e2b50a1c9ea38801723b2
LobashovaAnastasia <anastasia.lobashova27@gmail.com>
2023-12-07 00:37:21 +0300

    .

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -1,96 +1,68 @@
+import sys
+
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
-from .models import Tournament, Team, Player, News, Match
+
+from my_app.utils import query_debugger
+from my_app.models import Tournament, Team, Player, News, Match
 from datetime import datetime
+import logging
 
 
+logging.basicConfig(
+    format="%(asctime)s.%(msecs)03d %(levelname)s "
+    "[%(name)s:%(funcName)s:%(lineno)s] -> %(message)s",
+    datefmt="%Y-%m-%d,%H:%M:%S",
+    stream=sys.stdout,
+    level=logging.DEBUG,
+)
+logger = logging.getLogger(__name__)
+
+django_logger = logging.getLogger("django.db.backends")
+django_logger.setLevel(logging.DEBUG)
+django_logger.addHandler(logging.StreamHandler())
+
+
+@query_debugger(logger)
 def _get_all_tournaments():
-    queryset = Tournament.objects.select_related()
 
-    tournaments = []
-    for tournament in queryset:
-        tournaments.append(
-            {
-                'id': tournament.id,
-                'name': tournament.name,
-                'logo': tournament.logo
-            }
-        )
-
-    return tournaments
+    return list(Tournament.objects.select_related().values())
 
 
+@query_debugger(logger)
 def _get_all_teams():
-    queryset = Team.objects.select_related()
 
-    teams = []
-    for team in queryset:
-        teams.append(
-            {
-                'id': team.id,
-                'name': team.name,
-                'points': team.points,
-                'tournament': team.tournament,
-                'goals': team.goals,
-                'conceded': team.conceded,
-                'info': team.info,
-                'team_logo': team.team_logo,
-                'win': team.win,
-                'loss': team.loss,
-                'draw': team.draw
-            }
-        )
-
-    return teams
+    return list(Team.objects.select_related().values())
 
 
-def _get_last_two_news():
-    queryset = News.objects.select_related().order_by('-date')[:2]
-
-    return queryset
-
-
-def _all_news():
-    queryset = News.objects.select_related().order_by('-date')
-
-    return queryset
-
-
-def tournament_news(request: HttpRequest, tournament_id) -> HttpResponse:
-
+@query_debugger(logger)
+def get_tournament_news(request: HttpRequest, tournament_id) -> HttpResponse:
     news_list = News.objects.filter(category=tournament_id)
-
     data = {'news_list': news_list}
 
     return render(request, "news_list.html", context=data)
 
 
-def _get_last_two_matches():
-    queryset = Match.objects.filter(time__lte=datetime.now()).order_by('-time')[:2]
-
-    return queryset
-
-
+@query_debugger(logger)
 def main(request: HttpRequest) -> HttpResponse:
-    news_list = _get_last_two_news()
-
-    matches = _get_last_two_matches()
-
+    news_list = News.objects.select_related().order_by('-date')[:2]
+    matches = Match.objects.select_related().filter(time__lte=datetime.now()).order_by('-time')[:2]
     data = {'news_list': news_list, 'list_of_matches': matches}
 
     return render(request, "main.html", context=data)
 
 
-def all_news(request: HttpRequest) -> HttpResponse:
-    news_list = _all_news()
-
+@query_debugger(logger)
+def get_all_news(request: HttpRequest) -> HttpResponse:
+    news_list = News.objects.select_related().order_by('-date')
     data = {'news_list': news_list}
 
     return render(request, 'news_list.html', context=data)
 
 
-def news(request: HttpRequest, news_id: int) -> HttpResponse:
-    if not (news := News.objects.filter(id=news_id).first()):
+@query_debugger(logger)
+def get_news_by_id(request: HttpRequest, news_id: int) -> HttpResponse:
+    if not (news := News.objects.select_related().filter(id=news_id).first()):
         return HttpResponseNotFound(f"The news does not exist")
 
     data = {'news': news}
@@ -98,64 +70,59 @@ def news(request: HttpRequest, news_id: int) -> HttpResponse:
     return render(request, "news.html", context=data)
 
 
+@query_debugger(logger)
 def tournament_info(request: HttpRequest, tournament_id: int) -> HttpResponse:
     if not (tournament := Tournament.objects.filter(id=tournament_id).first()):
-        return HttpResponseNotFound(f"The tournament does not exist")
-
-    teams = Team.objects.filter(tournament=tournament).order_by('-points')
-
+        return HttpResponseNotFound(f"The tournament by id {tournament_id} does not exist")
+    teams = Team.objects.select_related().filter(tournament__pk=tournament_id).order_by('-points')
     data = {'tournament': tournament, 'teams': teams}
 
     return render(request, "tournament.html", context=data)
 
 
+@query_debugger(logger)
 def national_team(request: HttpRequest) -> HttpResponse:
-    tournament = Tournament.objects.get(name='Национальная сборная')
-
-    news_list = News.objects.filter(category=tournament)
-
-    matches = Match.objects.filter(first_team__in=tournament.teams.all()).order_by('time')[:4]
-
+    if not (tournament := Tournament.objects.filter(name='Национальная сборная').first()):
+        return HttpResponseNotFound(f"The national team does not exist")
+    news_list = News.objects.select_related().filter(category=tournament)
+    matches = Match.objects.select_related().filter(first_team__in=tournament.teams.all()).order_by('time')[:4]
     data = {'news_list': news_list, "tournament": tournament, "matches": matches}
 
     return render(request, "national_team.html", context=data)
 
 
+@query_debugger(logger)
 def team_info(request: HttpRequest, team_id: int) -> HttpResponse:
-    teams = Team.objects.filter(id=team_id).first()
-
+    if not (teams := Team.objects.filter(id=team_id).first()):
+        return HttpResponseNotFound(f"The team by id {team_id} does not exist")
     players = Player.objects.filter(team=teams)
-
     data = {'info': teams, 'players': players}
 
     return render(request, "team_info.html", context=data)
 
 
+@query_debugger(logger)
 def statistics(request: HttpRequest, tournament_id: int) -> HttpResponse:
-    players = Player.objects.filter(team__tournament_id=tournament_id).order_by('-goals')[:10]
-
+    players = Player.objects.select_related().filter(team__tournament_id=tournament_id).order_by('-goals')[:10]
     data = {"players": players}
 
     return render(request, "statistics.html", context=data)
 
 
+@query_debugger(logger)
 def list_of_matches(request: HttpRequest, tournament_id: int) -> HttpResponse:
-    tournament = Tournament.objects.get(id=tournament_id)
-
-    matches = Match.objects.filter(first_team__in=tournament.teams.all()).order_by('-time')
-
+    if not (tournament := Tournament.objects.filter(id=tournament_id).first()):
+        return HttpResponseNotFound(f"The tournament by id {tournament_id} does not exist")
+    matches = Match.objects.select_related().filter(first_team__in=tournament.teams.all()).order_by('-time')
     data = {'tournament': tournament, 'matches': matches}
 
     return render(request, "list_of_matches.html", context=data)
 
 
-def results(request: HttpRequest):
-    return HttpResponse("Results")
-
-
-def player(request: HttpRequest, player_id: int):
-    player_info = Player.objects.filter(id=player_id).first()
-
+@query_debugger(logger)
+def get_player_by_id(request: HttpRequest, player_id: int):
+    if not (player_info := Player.objects.select_related().filter(id=player_id).first()):
+        return HttpResponseNotFound(f"The player by id {player_id} does not exist")
     data = {'player': player_info}
 
     return render(request, "player_info.html", context=data)
